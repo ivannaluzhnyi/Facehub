860724ea8174ab4a8034dc38c8d20beb7dc2bc0d
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2019-07-04 14:08:47 +0200

    add api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,25 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('posts', function() {
+    return Post::all();
+});
+
+Route::get('posts/{id}', function($id) {
+    return Post::find($id);
+});
+
+Route::get('categories', function() {
+    return Category::all();
+});
+Route::get('category/{id}', function($id) {
+    return Category::find($id);
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'Auth\LoginController@login');
+});
